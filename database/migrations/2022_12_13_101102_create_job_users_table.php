9d4a5af1831d4415cdf0ab3205c458cb99062b9a
sonugoswami <sonugoswami98622@gmail.com>
2022-12-13 16:00:07 +0545

    JobUser create

---
+++ database/migrations/2022_12_13_101102_create_job_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Job;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_users', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(User::class)->constrained();
+            $table->foreignId(Job::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_users');
+    }
+};
