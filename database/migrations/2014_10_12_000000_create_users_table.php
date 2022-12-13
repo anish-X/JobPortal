d7dbcc3aec3e3e15392ec5993f4aa839842edea9
Arjun Regmi <arjunregmi3344@gmail.com>
2022-12-13 13:23:56 +0545

    Changes: adding field list in migration of user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,17 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('email')->unique();
+            $table->string('mobile_num')->unique();
+            $table->string('address')->nullable();
+            $table->integer('company_id')->nullable();
+            $table->enum('role',['super_admin','company_admin','manager','recruiters','employee'])->default('user');
+            $table->string('provider');
+            $table->integer('provider_id');
+            $table->boolean('status')->default(1);
+            $table->timestamp('email_verified_at')->nullable();
+           
             $table->rememberToken();
             $table->timestamps();
         });
