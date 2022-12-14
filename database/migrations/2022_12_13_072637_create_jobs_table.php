cf41365a63efcab6dd2d8cb69a7b4a626811bcf1
Anish Shrestha <077bct013@ioepc.edu.np>
2022-12-14 11:28:10 +0545

    Change minor issue on job table

--- database/migrations/2022_12_13_072637_create_jobs_table.php
+++ database/migrations/2022_12_13_072637_create_jobs_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\JobCategory;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,9 +16,9 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(Category::class)->constrained();
+            $table->foreignIdFor(CompanyCategory::class)->constrained();
             $table->foreignIdFor(User::class)->constrained();
-            $table->foreignIdFor(Job_Position::class)->constrained();
+            $table->foreignIdFor(JobCategory::class)->constrained();
             $table->UnsignedInteger('experience');
             $table->string('skills');
             $table->timestamps('deadline');
