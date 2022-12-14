eb3b4210ac6b7bc33718cb1c9d47a860745340f7
Anish Shrestha <077bct013@ioepc.edu.np>
2022-12-14 19:34:33 +0545

    worked on job mcr and job blade file

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Job extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'experience',
+        'skills',
+        'deadline',
+        'salary',
+        'vacancy',
+        'description'
+
+    ];
+
+    public function category(){
+        return $this->belongsTo(JobCategory::class);
+    }
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\JobCategoryController;
+use App\Http\Controllers\JobController;
+use App\Models\Job;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,3 +24,7 @@ Route::get('/', function () {
 Route::get('/company/create',[CompanyController::class, 'create'])->name('company.create');
 Route::post('/company/save',[CompanyController::class, 'save'])->name('company.save');
 
+Route::get('job/',[JobController::class,'index'])->name('job.index');
+Route::post('job/create', [JobController::class, 'store'])->name('job.create');
+Route::get('job/category', [JobCategoryController::class,'index'])->name('job.category');
+Route::post('job/category/create', [JobCategoryController::class, 'store'])->name('category.create');
